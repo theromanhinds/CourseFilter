1ca7e128e2ccdb77e1a99f5b230dc9644f9a8b5c
iamrch <theromanhinds17@gmail.com>
2024-12-20 17:41:41 -0500

    design part 2

--- src/Course.js
+++ src/Course.js
@@ -3,12 +3,23 @@ import React from 'react'
 function Course({course}) {
   return (
     <div className='Course'>
-        <h3>{course.subject} {course.courseNum}</h3>
-        <h3>{course.title}</h3>
-        <h3>{course.distSimple}</h3>
-        <h3>{course.days}</h3>
-        <h3>{course.writing}</h3>
-        <h3>{course.times}</h3>
+      <div className='SubjectNumber'>
+        <h3>{course.subject ? course.subject : "Subject Missing"}</h3> 
+        <p>{course.courseNum ? course.courseNum : "CourseNum Missing"}</p>
+      </div>
+      <div className='TitleInstructor'>
+        <h3>{course.title ? course.title : "Title Missing"}</h3> 
+        <p>{course.instructor ? course.instructor : "Instructor Missing"}</p>
+      </div>
+      <div className='DistWriting'>
+        <h3>{course.distSimple ? course.distSimple : "Dist Missing"}</h3> 
+        <p>{course.writing ? course.writing : "Writing Missing"}</p>
+      </div>
+      <div className='DaysTimes'>
+        <h3>{course.days ? course.days : "Days Missing"}</h3> 
+        <p>{course.times ? course.times : "Times Missing"}</p>
+      </div>
+        
     </div>
   )
 }
--- src/Filter.js
+++ src/Filter.js
@@ -4,12 +4,7 @@ function Filter() {
   return (
     <div className='FilterContainer'>
         <div className='FilterOptionsContainer'>
-            <h2>Filter1</h2>
-            <h2>Filter2</h2>
-            <h2>Filter3</h2>
-            <button>Button1</button>
-            <button>Button2</button>
-            <button>Button3</button>
+            
         </div>
     </div>
   )
